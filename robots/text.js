3728d86143869cbd94110ee0d19cd9c5f913e553
415andrew <44589963+415andrew@users.noreply.github.com>
2020-01-18 19:38:49 -0300

    Revert "feat: add google imagens search"

--- robots/text.js
+++ robots/text.js
@@ -27,8 +27,8 @@ async function robot() {
 	async function fetchContentFromWikipedia(content) {
 		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
 		const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-		const wikipeiaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
-		const wikipediaContent = wikipeiaResponse.get()
+		const wikipeiaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+		const wikipediaContent = wikipeiaResponde.get()
 	
 		content.sourceContentOriginal = wikipediaContent.content
 	}
